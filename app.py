b6a77ee051ef51e3c3d0e003656e6380cc057ff1
tcanbolat <kangalcanbolat@gmail.com>
2023-04-29 00:53:44 -0700

    refactored submit_vote()

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from common.common import food_list, get_latest_file
-from db.create_db import setup_db
+from db import create_db, vote
 from flask import Flask, render_template, request, make_response
 from io import BytesIO
 from keras.backend import clear_session  # REMOVE IMPORT IF USING tflite_runtime
 from keras.models import load_model  # REMOVE IMPORT IF USING tflite_runtime
 import os
 from predict import predict_class
-import sqlite3
 
 
 
@@ -57,52 +56,23 @@ def predict_image():
 
 @app.route('/vote', methods=['POST'])
 def submit_vote():
-
     new_vote = request.get_json()
 
     if 'correct' in new_vote:
-            col_name = 'correct'
+        col_name = 'correct'
     elif 'incorrect' in new_vote:
-            col_name = 'incorrect'
+        col_name = 'incorrect'
     else:
         response = make_response({'message': 'Could not cast vote.'}, 400)
         return response
 
-    conn = sqlite3.connect('db/votes.db')
-    cursor = conn.cursor()
-
-    cursor.execute("INSERT INTO Votes ({}) VALUES (?)".format(col_name),(True,))
-
-    conn.commit()
-
-    cursor.execute("SELECT COUNT(*) FROM Votes WHERE correct = 1")
-    correct_count = cursor.fetchone()[0]
-
-    cursor.execute("SELECT COUNT(*) FROM Votes WHERE incorrect = 1")
-    incorrect_count = cursor.fetchone()[0]
-
-    conn.close()
-
-    total_count = correct_count + incorrect_count
-
-    if total_count == 0:
-        accuracy = 'N/A'
-    else:
-         accuracy = (correct_count / total_count) * 100
-         accuracy = '{:.0f}%'.format(accuracy)
-
-    data = {
-        'correct_count': correct_count,
-        'incorrect_count': incorrect_count,
-        'total_count': total_count,
-        'accuracy': accuracy
-    }
+    data = vote.submit_vote(col_name)
 
     return make_response({'message': 'New vote submitted successfully', 'result': data}, 201)
 
 
 if not os.path.exists('db/votes.db'):
-    setup_db()
+    create_db.setup_db()
 
 clear_session()  # REMOVE STATMENT IF USING tflite_runtime
 

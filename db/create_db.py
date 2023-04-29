ef5d606974cd4c68b1d614ab9e7f6b3b32919721
tcanbolat <kangalcanbolat@gmail.com>
2023-04-28 20:09:50 -0700

    update

--- db/create_db.py
+++ db/create_db.py
@@ -1,16 +1,26 @@
 import sqlite3
 import os
 
-if not os.path.exists('votes.db'):
-    open('votes.db', 'w').close()
+def setup_db():
+    if not os.path.exists('votes.db'):
+        open('votes.db', 'w').close()
 
-conn = sqlite3.connect('votes.db')
-cursor = conn.cursor()
+    conn = sqlite3.connect('votes.db')
+    cursor = conn.cursor()
 
-with open('schema.sql', 'r') as f:
-    schema = f.read()
-    cursor.executescript(schema)
+    print('Created votes.db')
 
-conn.commit()
-conn.close()
-print('_DB_Created_')
+    with open('schema.sql', 'r') as f:
+        schema = f.read()
+
+    # Check if the Votes table exists
+    cursor.execute("PRAGMA table_info('Votes')")
+    table_info = cursor.fetchall()
+    if len(table_info) > 0:
+        print("The Votes table already exists in the database")
+    else:
+        cursor.executescript(schema)
+        print("The Votes table was created successfully")
+
+    conn.commit()
+    conn.close()

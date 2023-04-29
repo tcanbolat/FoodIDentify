15a211bd4c00e2d5013d5b2443bf84307524ac43
tcanbolat <kangalcanbolat@gmail.com>
2023-04-28 22:29:09 -0700

    added predict function for tflite_runtime

--- predict.py
+++ predict.py
@@ -23,3 +23,41 @@ def predict_class(model, image):
 
     return {"prediction": pred_value, "confidence": percentage}
 
+
+### PREDICTION FUNCTION to be used with tflite_runtime
+### Pip install tflite_runtime
+### DOES_NOT_WORK WITH PYTHON 3.11 OR 3.10 - USE PYTHON 3.9 OR 3.6
+
+# import numpy as np
+# from PIL import Image
+
+# from common.common import food_list
+
+# def predict_class(interpreter, image_bytes):
+
+#     interpreter.allocate_tensors()
+
+#     # Get input and output tensors
+#     input_details = interpreter.get_input_details()
+#     output_details = interpreter.get_output_details()
+
+#     # Load image and resize to model input shape
+#     img = Image.open(image_bytes).convert('RGB').resize((input_details[0]['shape'][1], input_details[0]['shape'][2]), resample=Image.BILINEAR)
+#     img = np.array(img).astype(np.float32) / 255.0
+
+#     # Set input tensor
+#     interpreter.set_tensor(input_details[0]['index'], np.expand_dims(img, axis=0))
+
+#     # Run inference
+#     interpreter.invoke()
+
+#     # Get output tensor and index of predicted class
+#     output_data = interpreter.get_tensor(output_details[0]['index'])
+#     pred_index = np.argmax(output_data)
+
+#     # Get predicted class name and confidence
+#     food_list.sort()
+#     pred_value = food_list[pred_index]
+#     percentage = "{:.0%}".format(output_data[0][pred_index])
+
+#     return {"prediction": pred_value, "confidence": percentage}

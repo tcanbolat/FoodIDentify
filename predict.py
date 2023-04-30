992e7dd23a33c0031d3fa056b602332de745845a
Abdullah Canbolat <55068433+tcanbolat@users.noreply.github.com>
2023-04-29 17:43:06 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -31,6 +31,8 @@ def predict_class(model, image, food_list):
 
 # def predict_class(interpreter, image_bytes, food_list):
 
+#     interpreter.allocate_tensors()
+
 #     # Get input and output tensors
 #     input_details = interpreter.get_input_details()
 #     output_details = interpreter.get_output_details()
